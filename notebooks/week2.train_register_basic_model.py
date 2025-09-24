0d80f0b65a049887bbb8ab056d093d27a7ae5afa
srinis98 <srinis98@gmail.com>
2025-09-24 11:17:56 +0200

    initialise week2

---
+++ notebooks/week2.train_register_basic_model.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+import mlflow
+from pyspark.sql import SparkSession
+
+from term_deposit.config import ProjectConfig, Tags
+
+from dotenv import load_dotenv
+from term_deposit.utils import is_databricks
+import os
+# COMMAND ----------
+# If you have DEFAULT profile and are logged in with DEFAULT profile,
+# skip these lines
+
+if not is_databricks():
+    load_dotenv()
+    profile = os.environ.get("PROFILE", "DEFAULT")
+    mlflow.set_tracking_uri(f"databricks://{profile}")
+    mlflow.set_registry_uri(f"databricks-uc://{profile}")
+
+config = ProjectConfig.from_yaml(config_path="../project_config.yml", env="dev")
+spark = SparkSession.builder.getOrCreate()
+tags = Tags(**{"git_sha": "abcd12345", "branch": "week_2"})
+# COMMAND ----------
---
+++ src/term_deposit/utils.py
@@ -0,0 +1,10 @@
+"""Utility class."""
+
+import os
+
+def is_databricks() -> bool:
+    """Check if the code is running in a Databricks environment.
+
+    :return: True if running in Databricks, False otherwise.
+    """
+    return "DATABRICKS_RUNTIME_VERSION" in os.environ
